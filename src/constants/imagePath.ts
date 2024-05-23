e76e4c9757998ee52f9457cf7ab9c2761ef18b06
huyenbtk <khanhhuyensthp@gmail.com>
2024-05-23 17:05:26 +0700

    update footer

--- src/constants/imagePath.ts
+++ src/constants/imagePath.ts
@@ -6,21 +6,25 @@ import LOGO_BOARDING from "public/images/logos/pet boarding.png";
 import LOGO_HEALTHCARE from "public/images/logos/healthcare.png";
 import LOGO_GROOMING from "public/images/logos/grooming.png";
 
+import LOGO from "public/images/logos/logo.png";
 import NEW_PET from "public/images/products/cat.png";
 import YOUR_PET from "public/images/products/dog.png";
 import PET_HEALTH from "public/images/products/dogspa.png";
 
 import BG_LANDING from "public/images/backgrounds/bg-landing.png";
+import BG_FOOTER from "public/images/backgrounds/bg footer.png";
 export const imagePath = {
   BANNERLANDING,
   BANNERLANDING2,
   BANNERLANDING3,
 
+  LOGO,
   LOGO_BOARDING,
   LOGO_HEALTHCARE,
   LOGO_GROOMING,
 
   BG_LANDING,
+  BG_FOOTER,
 
   NEW_PET,
   YOUR_PET,
